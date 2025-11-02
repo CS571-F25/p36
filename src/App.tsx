54d4101f572c63bf3cf8525876998ae1ad3b547d
Pranav Grandhi <pranav@grandhi.net>
2025-11-02 13:27:10 -0600

    fix router basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Layout from "./components/layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { index: true, lazy: () => import("./pages/home") },
-      { path: ":id", lazy: () => import("./pages/set") },
-      { path: "create", lazy: () => import("./pages/create") },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { index: true, lazy: () => import("./pages/home") },
+        { path: ":id", lazy: () => import("./pages/set") },
+        { path: "create", lazy: () => import("./pages/create") },
+      ],
+    },
+  ],
+  { basename: "/p36/" },
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
