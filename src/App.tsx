3bf81a34450534862edf74ad131e9b3ce7b47183
ericvang <ericvang100@gmail.com>
2025-11-18 20:17:02 -0600

    Fix routing for GitHub Pages: Add basename /p36 to router

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,40 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 
 import Layout from "./components/layout";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { 
+          index: true, 
+          lazy: async () => {
+            const module = await import("./pages/home");
+            return { Component: module.default };
+          }
+        },
+        { 
+          path: "create", 
+          lazy: async () => {
+            const module = await import("./pages/StudySets");
+            return { Component: module.default };
+          }
+        },
+        { 
+          path: "sets", 
+          lazy: async () => {
+            const module = await import("./pages/StudySession");
+            return { Component: module.default };
+          }
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <Layout />,
-    children: [
-      { 
-        index: true, 
-        lazy: async () => {
-          const module = await import("./pages/home");
-          return { Component: module.default };
-        }
-      },
-      { 
-        path: "create", 
-        lazy: async () => {
-          const module = await import("./pages/StudySets");
-          return { Component: module.default };
-        }
-      },
-      { 
-        path: "sets", 
-        lazy: async () => {
-          const module = await import("./pages/StudySession");
-          return { Component: module.default };
-        }
-      },
-    ],
-  },
-]);
+    basename: "/p36",
+  }
+);
 
 export default function App() {
   return <RouterProvider router={router} />;
